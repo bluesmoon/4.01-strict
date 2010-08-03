1ec5bfb4f14b2fc05108505e35803c32fe9564b4
Philip Tellis <philip@bluesmoon.info>
2010-08-03 00:30:59 -0700

    table to store links to flickr

---
+++ sql/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE If Not Exists strip (
+	n		INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
+	date_posted	TIMESTAMP NOT NULL,
+	id		VARCHAR(255) NOT NULL,
+	farm		SMALLINT NOT NULL,
+	secret		VARCHAR(255) NOT NULL,
+	server		VARCHAR(255) NOT NULL,
+	title		TEXT NOT NULL,
+	description	TEXT NULL,
+
+	PRIMARY KEY (n),
+	INDEX (date_posted)
+) Engine=InnoDB, CHARACTER SET=utf8, COLLATE=utf8_unicode_ci;
